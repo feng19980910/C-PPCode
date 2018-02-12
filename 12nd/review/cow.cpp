5dc4a376fec12a1207aadf34a53f8710f5302ae4
Feng <feng19980910@foxmail.com>
2018-02-12 12:21:47 +0800

    add the review and the first question with trouble

---
+++ 12nd/review/cow.cpp
@@ -0,0 +1,47 @@
+#include "cow.h"
+#include <iostream>
+#include <string.h>
+
+Cow::Cow() {
+	hobby = new char[1];
+	hobby[0] = '\0';
+	name[0] = '\0';
+	weight = 0;
+}
+
+Cow::Cow(const char * nm, const char * ho, double wt) {
+	delete[] hobby;
+	hobby = new char[strlen(ho) + 1];
+	strcpy(hobby, ho);
+	strcpy(name, nm);
+	weight = wt;
+}
+
+Cow::Cow(const Cow & c) {
+	delete[] hobby;
+	hobby = new char[strlen(c.hobby) + 1];
+	strcpy(hobby, c.hobby);
+	strcpy(name, c.name);
+	weight = c.weight;
+}
+
+Cow::~Cow() {
+	delete[] hobby;
+}
+
+Cow & Cow::operator=(const Cow & c) {
+	delete[] hobby;
+	hobby = new char[strlen(c.hobby) + 1];
+	strcpy(hobby, c.hobby);
+	strcpy(name, c.name);
+	weight = c.weight;
+	return *this;
+}
+
+void Cow::ShowCow() const {
+	using std::cout;
+	using std::endl;
+	cout << "Cow " << name << "'s hobby is : " << endl;
+	cout << hobby << endl;
+	cout << "Weight : " << weight << endl;
+}
---
+++ 12nd/review/cow.h
@@ -0,0 +1,14 @@
+// cow.h -- declaration of class Cow
+class Cow {
+	private:
+		char name[20];
+		char * hobby;
+		double weight;
+	public:
+		Cow();
+		Cow(const char * nm, const char * ho, double wt);
+		Cow(const Cow & c);
+		~Cow();
+		Cow & operator=(const Cow & c);
+		void ShowCow() const;		// display all cow data
+};
